1f8494a560f6034e5f535b4541da61df513510b6
viandwi24 <fiandwi0424@gmail.com>
2020-05-04 17:28:02 +0700

    Added Command Full Generate Module Folder

--- src/Commands/MakeModuleCommand.php
+++ src/Commands/MakeModuleCommand.php
@@ -12,7 +12,7 @@ class MakeModuleCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'make:module {name : The Name of Module}';
+    protected $signature = 'make:module {name : The Name of Module} {--f|full : Make full module with folder, controller, dll}';
 
     /**
      * The console command description.
@@ -42,6 +42,7 @@ class MakeModuleCommand extends Command
         $namespace = str_replace(' ', '', $this->argument('name'));
         $name = $this->argument('name');
         $service = $namespace . "ServiceProvider";
+        // return $this->info($namespace);
 
         // make a folder
         $path = Module::getPath() . '/' . $name;
@@ -64,10 +65,30 @@ class MakeModuleCommand extends Command
 
         // make a service
         $this->makeService($name, $namespace, $service, $path);
+
+        // check full module generate?
+        $full = $this->option('full');
+        if ($full)
+        {
+            $this->makeController($name, $namespace, $path);
+            $this->makeAdditional($name, $namespace, $path);
+        }
+
+
+        // success
         $this->info('Module "' . $name . '" Created in (' . $path . ')');
     }
 
 
+    /**
+     * Make service provider from stubs
+     *
+     * @param string $name
+     * @param string $namespace
+     * @param string $service
+     * @param string $path
+     * @return void
+     */
     protected function makeService($name, $namespace, $service, $path)
     {
         $serviceTemplate = str_replace(
@@ -79,8 +100,66 @@ class MakeModuleCommand extends Command
         file_put_contents($servicePath , $serviceTemplate);
     }
 
+
+
+    /**
+     * Make Controller from stub
+     *
+     * @param string $name
+     * @param string $namespace
+     * @param string $path
+     * @return void
+     */
+    protected function makeController($name, $namespace, $path)
+    {
+        // make folder
+        mkdir($path . '/Controllers');
+        
+        // make controller file
+        $name = $name . 'Controller';
+        $controllerTemplate = str_replace(
+            [ '{{controllerName}}', '{{controllerNamespace}}', ], 
+            [ $name, $namespace ], 
+            $this->stub('controller')
+        );
+        $controllerPath = $path . '/Controllers//' . $name . '.php';
+        file_put_contents($controllerPath , $controllerTemplate);
+    }
+
+
+
+    protected function makeAdditional($name, $namespace, $path)
+    {
+        // make views
+        mkdir($path . '/views');
+
+        // make migrations
+        mkdir($path . '/migrations');
+
+        // make models
+        mkdir($path . '/Models');
+
+
+        // make routes
+        $routesTemplate = str_replace(
+            [ '{{moduleName}}', '{{moduleNameLower}}' ], 
+            [ $name, strtolower($name) ], 
+            $this->stub('routes')
+        );
+        $routesPath = $path . '/routes' . '.php';
+        file_put_contents($routesPath , $routesTemplate);
+    }
+
+
+
+    /**
+     * retrieve a stub
+     *
+     * @param string $name
+     * @return void
+     */
     protected function stub($name)
     {
-        return file_get_contents(__DIR__ . '/../../stubs/'. $name .'.stubs');
+        return file_get_contents(__DIR__ . '/../../stubs/'. $name .'.stub');
     }
 }
